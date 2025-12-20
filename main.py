e642f2d58b0ef4cd51d3dcc66ee9092c1479fc31
echobloomsite-gif <echobloomsite@gmail.com>
2025-12-20 23:02:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def send_mail(From,to,content,Subject):
     try:
         with smtplib.SMTP("smtp.gmail.com",587) as server:
             server.starttls()
-            server.login(From,"jfir swho dezc ofad")
+            server.login(From,os.getenv("EMAIL_PASSWORD"))
             server.send_message(message)
             print("Email_Envoyé avec succès ")
     except Exception as e:
